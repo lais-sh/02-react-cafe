1e82e258ef154e8cd66739b0ff951be79c76eb2d
lais-sh <lithshamok@gmail.com>
2025-06-08 12:12:58 +1000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-});
+  base: '/02-react-cafe/',
+})
